b4456f88728378736994fd7fd0a7177734674742
Emiliano Bazan Zapata <emilianobz546@gmail.com>
2021-08-25 23:31:43 -0300

    Aplicando el Salto al Personaje

--- GameIn2D/Assets/Scripts/PlayerController.cs
+++ GameIn2D/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,36 @@
-﻿using System.Collections;
+﻿using System.Numerics;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //fuerza de salto
+    public float JumpForce = 5f;
+    //rigidbody del personaje que usare para saltar
+    private Rigidbody2D rigidBody;
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         
     }
-
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //aqi el usuario acaba de pulsar la tecla espacio
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        //f = m * a
+        rigidBody.AddForce(UnityEngine.Vector2.up * JumpForce, ForceMode2D.Impulse);
     }
 }
