c1b29af97f11f6a05ec6cbce36a4c34d1c97894b
Emiliano Bazan Zapata <emilianobz546@gmail.com>
2021-08-26 21:12:39 -0300

    Configurando los Estados del Juego

--- GameIn2D/Assets/Scripts/GameManager.cs
+++ GameIn2D/Assets/Scripts/GameManager.cs
@@ -1,19 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//enumerado
+//posibles estado del juego
+public enum GameState 
+{
+    Menu,
+    InGame,
+    GameOver
+}
 
 public class GameManager : MonoBehaviour
 {
+    //variable para indicar en que estado se encuentra el juego
+    public GameState CurrentGameState = GameState.Menu;
     //metodo para iniciar el juego
     public void InciarPartida() 
     {
+        SetGameState(GameState.InGame);
     }
     //metodo que se llamara al morir
     public void GameOver() 
     {
+        SetGameState(GameState.GameOver);
     }
     //metodo que llamara para volver al menu
     public void BackToMenu() 
     {
+        SetGameState(GameState.Menu);
+    }
+    //metodo encargado de cambiar el estado del juego
+    private void SetGameState(GameState NewGameState) 
+    {
+        if (NewGameState == GameState.Menu) 
+        {
+            //codigo para mostrar el menu
+        }
+        else if (NewGameState == GameState.InGame)
+        {
+            //codigo para juegar
+        }
+        else if (NewGameState == GameState.GameOver)
+        {
+            //codigo para mostrar la escena de muerte
+        }
+
+        //asignamos el estado de juego actual desde el parametro
+        this.CurrentGameState = NewGameState;
     }
 }
