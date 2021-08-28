11d3ff4cc143e2d7e50d8c7fc6a6d81d733fc534
Emiliano Bazan Zapata <emilianobz546@gmail.com>
2021-08-28 19:14:50 -0300

    Generando los Bloques Inciales en el Nivel

--- GameIn2D/Assets/Scripts/LevelGenerator.cs
+++ GameIn2D/Assets/Scripts/LevelGenerator.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static LevelGenerator SharedInstance;
+    public List<LevelBlock> AllTheLevelBlocks = new List<LevelBlock>();
+    public Transform LevelStartPoint;
+    public List<LevelBlock> CurrentBlocks = new List<LevelBlock>();
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        SharedInstance = this;
+    }
+    private void Start() 
+    {
+        AddLevelBlock();
+        AddLevelBlock();
+        AddLevelBlock();
+    }
+    public void AddLevelBlock() 
+    {
+        //valor aleatorio entre 0 y la cantidad de niveles creados
+        int RandomIndex = Random.Range(0, AllTheLevelBlocks.Count);
+        //instanciamos un bloque del nivel desde la carpeta assets hasta la escena
+        LevelBlock CurrentBlock = (LevelBlock)Instantiate(AllTheLevelBlocks[RandomIndex]);
+        //pone el bloque del nivel como hijo de lLevelGenerator
+        CurrentBlock.transform.SetParent(this.transform, false);
+
+        Vector3 SpawPosition = Vector3.zero;
+        if (CurrentBlocks.Count == 0)
+        {
+            SpawPosition = LevelStartPoint.position;
+        }
+        else 
+        {
+            SpawPosition = CurrentBlocks[CurrentBlocks.Count - 1].ExitPoint.position;
+        }
+        //corregimos la posicion del bloque restando las posiciones
+        Vector3 Correction = new Vector3(SpawPosition.x - CurrentBlock.StartPoint.position.x,
+                                         SpawPosition.y - CurrentBlock.StartPoint.position.y, 0);
+
+        Debug.Log(Correction);
+        CurrentBlock.transform.position = Correction;
+        CurrentBlocks.Add(CurrentBlock);
+
+    }
+    public void RemoveLevelBlock() 
+    {
+
+    }
+    public void RemoveAllThrBlock()
+    {
+
+    }
+    public void GenerateInitialBlock() 
+    {
+
     }
 }
