b803fd579434695888220fdb03f0dd23dd8e64ee
EmilianoBazanZapata <57721403+EmilianoBazanZapata@users.noreply.github.com>
2021-09-14 20:27:58 -0300

    Mostrar el número de monedas recolectadas por pantalla

---
+++ GameIn2D/Assets/Scripts/ViewInGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ViewInGame : MonoBehaviour
+{
+    //etiqueta para ver la puntuacion actual
+    public Text CollectableLabel;
+
+    public Text ScoreLabel;
+
+    public Text MaxScoreLabel;
+    private void Update()
+    {
+        if (GameManager.SharedInstance.CurrentGameState == GameState.InGame) 
+        {
+            float CurrentObjects  = GameManager.SharedInstance.CollectedObjects;
+            //mostrare en el canvas la cantidad de monedas recogidas
+            this.CollectableLabel.text = CurrentObjects.ToString(); 
+
+        }
+    }
+}
