ec4b42e24fe3c9e2fdc09a48d8fa17b138b85723
EmilianoBazanZapata <57721403+EmilianoBazanZapata@users.noreply.github.com>
2021-09-14 12:43:22 -0300

    Creando los Metodos para el Coleccionable

---
+++ GameIn2D/Assets/Scripts/Collectable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    //variable para saber si la variable fue recogida o no
+    bool IsCollectred = false;
+    //metodo para activar la moneda
+    private void Show()
+    {
+
+    }
+    //metodo para desactivar la moneda
+    private void Hide()
+    {
+
+    }
+    //metodo para colectar la moneda
+    private void Collect()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        
+    }
+}
