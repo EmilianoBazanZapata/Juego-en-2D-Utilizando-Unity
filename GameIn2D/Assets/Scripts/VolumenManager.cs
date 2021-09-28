d61fd6faaf62daaf258d56f75f37698245cc45fe
EmilianoBazanZapata <57721403+EmilianoBazanZapata@users.noreply.github.com>
2021-09-28 10:33:42 -0300

    Agregando el Control del Volumen

---
+++ GameIn2D/Assets/Scripts/VolumenManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumenManager : MonoBehaviour
+{
+    //hace referencia al slider en pantalla
+    public Slider slider;
+    //variable para manejar el volumen
+    public float sliderValue;
+
+    private void Start() {
+        //guardamos el volumen para que se mantengan los cambios
+        slider.value = PlayerPrefs.GetFloat("VolumenAudio",5f);
+        //el volumen del juego tomara el valor del slider
+        AudioListener.volume = slider.value;
+    }
+    public void ChangeSlider(float valor)
+    {
+        sliderValue = valor;
+        PlayerPrefs.SetFloat("VolumenAudio",sliderValue);
+        AudioListener.volume = slider.value;
+    }
+
+}
