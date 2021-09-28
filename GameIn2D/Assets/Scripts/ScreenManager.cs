14fa9dd1f5f698a154b1611d8811ed5ead4d4e65
EmilianoBazanZapata <57721403+EmilianoBazanZapata@users.noreply.github.com>
2021-09-28 11:34:23 -0300

    Agregando la Funcion de pantalla Completa

---
+++ GameIn2D/Assets/Scripts/ScreenManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenManager : MonoBehaviour
+{
+    public Toggle toggle;
+    private void Start() {
+        if(Screen.fullScreen)
+        {
+            toggle.isOn = true;
+        }
+        else
+        {
+            toggle.isOn = false;
+        }
+    }
+    public void ActualizarPantallaCompleta(bool pantallaCompleta)
+    {
+        Screen.fullScreen = pantallaCompleta;
+    }
+}
