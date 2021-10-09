c7264f5f42e677830f9796abcb4cffc961c0a120
EmilianoBazanZapata <57721403+EmilianoBazanZapata@users.noreply.github.com>
2021-10-09 20:21:47 -0300

    Agregando diferentes temas para el sonido del juego

---
+++ GameIn2D/Assets/Scripts/PlayRandomSound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayRandomSound : MonoBehaviour
+{
+    public AudioSource _As;
+    //arreglo para agregar los temas
+    public AudioClip[] AudioClipArray;
+
+    private void Awake() {
+        _As = GetComponent<AudioSource>();
+    }
+
+    private void Start() {
+        _As.clip = AudioClipArray[Random.Range(0,AudioClipArray.Length)];
+    }
+}
