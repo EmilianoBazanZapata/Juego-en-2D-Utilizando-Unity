c3a3a831f9db5f66e461629f29cf053f33c26932
EmilianoBazanZapata <57721403+EmilianoBazanZapata@users.noreply.github.com>
2021-10-09 22:09:30 -0300

    Agregando un Array de Temas para poder cambiar el sonido de fundo a gusto

--- GameIn2D/Assets/Scripts/PlayRandomSound.cs
+++ GameIn2D/Assets/Scripts/PlayRandomSound.cs
@@ -8,11 +8,51 @@ public class PlayRandomSound : MonoBehaviour
     //arreglo para agregar los temas
     public AudioClip[] AudioClipArray;
 
-    private void Awake() {
-        _As = GetComponent<AudioSource>();
-    }
+    private int TemaActual = 14;
 
-    private void Start() {
-        _As.clip = AudioClipArray[Random.Range(0,AudioClipArray.Length)];
+    private void Start()
+    {
+        //_As.clip = AudioClipArray[Random.Range(0,AudioClipArray.Length)];
+        _As = this.GetComponent<AudioSource>();
+        _As.clip = AudioClipArray[14];
+        _As.Play();
+        _As.loop = true;
+    }
+    public void TemaSiguiente()
+    {
+        _As.Pause();
+        if (TemaActual == 16)
+        {
+            TemaActual = 0;
+            _As.clip = AudioClipArray[TemaActual];
+            _As.Play();
+            _As.loop = true;
+        }
+        else
+        {
+            TemaActual++;
+            _As.clip = AudioClipArray[TemaActual];
+            _As.Play();
+            _As.loop = true;
+        }
+        //Debug.Log(TemaActual);
+    }
+    public void TemaAnterior()
+    {
+        _As.Pause();
+        if (TemaActual == 0)
+        {
+            TemaActual = 16;
+            _As.clip = AudioClipArray[TemaActual];
+            _As.Play();
+            _As.loop = true;
+        }
+        else
+        {
+            TemaActual--;
+            _As.clip = AudioClipArray[TemaActual];
+            _As.Play();
+            _As.loop = true;
+        }
     }
 }
